f447efe6c1e6e613bacf1af3a68fa480ad9b99cd
tomtom-berlin <tomtom-berlin@users.noreply.github.com>
2024-11-20 01:36:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ while utime.ticks_ms() - t < 3000:  # 3 Sekunden warten auf evtl. Unterbrechung 
     if rp2.bootsel_button():
         raise(RuntimeError("BOOTSEL - Abbruch"))
 
-import op_test.py
+import op_test
 
     
     
